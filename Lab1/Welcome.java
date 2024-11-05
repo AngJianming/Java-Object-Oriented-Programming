bc083084e3305d40ddb5b2512f69f50d62fac67f
AngJianming <angjianming2005@hotmail.com>
2024-11-05 17:41:36 +0800

    updates

---
+++ Lab1/Welcome.java
@@ -0,0 +1,8 @@
+public class Welcome { 
+    /* @param Welcome to Java */ 
+    public static void main(String[] args){
+        System.out.println("Welcome to Java.");
+        System.out.println("My first program");
+        
+    };
+}
---
+++ Lab2/Exercise_1_FahrenheitToCelsius.java
@@ -0,0 +1,13 @@
+package Lab2;
+
+import java.util.Scanner;
+
+public class Exercise_1_FahrenheitToCelsius {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Fahrenheit: ");
+        double fahrenheit = input.nextDouble();
+        double celsius = (fahrenheit - 32) * 5 / 9;
+        System.out.println("Celsius: " + celsius);
+    }
+}
